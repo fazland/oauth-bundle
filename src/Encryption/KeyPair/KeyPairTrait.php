280a4a2b342668718a8c4102b335da5f880d5a6e
Massimiliano Braglia <massimiliano.braglia@gmail.com>
2019-01-31 14:44:51 +0100

    Introduced KeyPairTrait

---
+++ src/Encryption/KeyPair/KeyPairTrait.php
@@ -0,0 +1,68 @@
+<?php declare(strict_types=1);
+
+namespace Fazland\OAuthBundle\Encryption\KeyPair;
+
+use Fazland\OAuthBundle\Enum\SignatureAlgorithm;
+
+/**
+ * Trait for common implementation of the KeyPairInterface.
+ */
+trait KeyPairTrait
+{
+    /**
+     * @var string
+     */
+    private $privateKey;
+
+    /**
+     * @var string
+     */
+    private $publicKey;
+
+    /**
+     * @var SignatureAlgorithm
+     */
+    private $signatureAlgorithm;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getPublicKey(): string
+    {
+        return $this->publicKey;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getPrivateKey(): string
+    {
+        return $this->privateKey;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getSignatureAlgorithm(): SignatureAlgorithm
+    {
+        return $this->signatureAlgorithm;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function resetKeyPair(): void
+    {
+        $resource = \openssl_pkey_new([
+            'curve_name' => 'secp521r1',
+            'private_key_type' => OPENSSL_KEYTYPE_EC,
+        ]);
+
+        \openssl_pkey_export($resource, $priKey);
+        $pubKey = \openssl_pkey_get_details($resource)['key'];
+
+        $this->signatureAlgorithm = SignatureAlgorithm::ES256();
+        $this->privateKey = $priKey;
+        $this->publicKey = $pubKey;
+    }
+}
